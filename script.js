08f60fc0067b7a02c5561330c7eac297ba7bdc08
Loki-Malone <60401309+Loki-Malone@users.noreply.github.com>
2020-03-05 16:13:31 -0600

    created lets for cQ, aA, score, qC, and aQ

--- script.js
+++ script.js
@@ -2,4 +2,8 @@ var question = document.getElementById('#question');
 var choices = Array.from(document.getElementsByClassName('choice-text'));
 console.log(choices);
 
-
+let currentQuestions = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let avaiableQuestions =[];
